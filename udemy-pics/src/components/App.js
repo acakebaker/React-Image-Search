0850224f24119ea4e1f1d477cdf625eacb1c96ef
Alexander Baker <38450939+acakebaker@users.noreply.github.com>
2022-01-18 20:36:43 -0600

    Added Axios

--- udemy-pics/src/components/App.js
+++ udemy-pics/src/components/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
-  };
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID QGdfGxOzAdvcR2TScvGB-zr8KB9Z3xlyBqICIbE_aqA',
+      }
+    });
+    console.log(response.data.results);
+  }
 
   render() {
     return (
       <div className='ui container' style={{ marginTop: '10px' }}>
         <SearchBar onSearchSubmit={this.onSearchSubmit} />
+        <ImageList />
       </div>
     );
   }
--- udemy-pics/src/components/ImageList.js
+++ udemy-pics/src/components/ImageList.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ImageList = () => {
   return (
     <div>
-      
+      <h3>Results</h3>
     </div>
   );
 };
